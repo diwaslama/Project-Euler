ef1ac7043dd64b18e1135862eebb14b33dc481c2
diwaslama <lamadiwash9@gmail.com>
2024-01-06 18:46:04 +1100

    Problem 2 solution

--- Project euler/Problem2.py
+++ Project euler/Problem2.py
@@ -2,3 +2,18 @@
 # By considering the terms in the Fibonacci sequence 
 # whose values do not exceed four million,
 # find the sum of the even-valued terms
+
+def sum_of_even_fibonacci(limit):
+    a = 1
+    b = 2
+    total_sum = 0
+
+    while b <= limit:
+        if b % 2 == 0:
+            total_sum += b
+        a, b = b, a + b
+
+    return total_sum
+
+answer = sum_of_even_fibonacci(4000000)
+print(answer)
