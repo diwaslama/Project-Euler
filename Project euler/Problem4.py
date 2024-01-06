43283856d0e894eda1d8e43e9c109365097e6c6d
diwaslama <lamadiwash9@gmail.com>
2024-01-06 18:54:49 +1100

    Problem 4 Solution

--- Project euler/Problem4.py
+++ Project euler/Problem4.py
@@ -1,2 +1,17 @@
 #  Largest Palindrome Product
 #  Find the largest palindrome made from the product of two 3-digit numbers.
+
+def check_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+def largest_palindrome_product():
+    largest_palindrome = 0
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            product = i * j
+            if check_palindrome(product) and product > largest_palindrome:
+                largest_palindrome = product
+    return largest_palindrome
+
+answer = largest_palindrome_product()
+print(answer)
