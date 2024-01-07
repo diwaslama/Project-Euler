7f451f67942d8eeb325a0aec1eb4928d0e93101e
diwaslama <lamadiwash9@gmail.com>
2024-01-07 19:14:49 +1100

    Problem6 Solution

--- Project euler/Problem6.py
+++ Project euler/Problem6.py
@@ -2,3 +2,14 @@
 # Find the difference between the sum of the squares of the first 
 # one hundred natural numbers and the square of the sum
 
+n = 100
+
+# Sum of the squares of the first n natural numbers
+sum_of_squares = n * (n + 1) * (2 * n + 1) / 6
+
+# Square of the sum of the first n natural numbers
+square_of_sum = (n * (n + 1) / 2) ** 2
+
+answer = square_of_sum - sum_of_squares
+
+print(int(answer))
